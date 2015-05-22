734fb94bba6b047c3b12fa084ef13f7509f344f5
Tomas S. Grigera <tgrigera@iflysib.unlp.edu.ar>
2015-05-22 17:17:50 -0300

    Corrected typo in readbt.sh that caused failure

--- blib/readbt.sh
+++ blib/readbt.sh
@@ -2,6 +2,6 @@
 base=$(mktemp XXXXX)
 rm -f $base
 mkdir $base
-csplit -s -z -f $base/xx - '%===== BACKTRACE START ==========%1' '/===== BACKTRACE END ==========/'
+csplit -s -z -f $base/xx - '%===== BACKTRACE START ==========%' '/===== BACKTRACE END ==========/'
 sed '/\[.*\]/ {s/.*\[\(.*\)\].*/\1/}' ${base}/xx00 | addr2line -C -e $1 -f -i
 rm -rf ${base}
